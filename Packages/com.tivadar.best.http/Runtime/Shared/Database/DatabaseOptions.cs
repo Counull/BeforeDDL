64f0a8d69afc6f91b8b5f9733d92aa1eac3798b6
liuh <counull@163.com>
2024-09-01 21:15:59 +0800

    chore: Support BestHttp2 and grpc-dotnet-unity
    - Support BestHttp2 v3 using a fork of grpc-dotnet-unity from [grpc-dotnet-unity](https://github.com/ergatta/grpc-dotnet-unity/tree/master)

---
+++ Packages/com.tivadar.best.http/Runtime/Shared/Database/DatabaseOptions.cs
@@ -0,0 +1,12 @@
+namespace Best.HTTP.Shared.Databases {
+    public class DatabaseOptions {
+        public string Name;
+        public bool UseHashFile;
+
+        public DiskManagerOptions DiskManager = new DiskManagerOptions();
+
+        public DatabaseOptions(string dbName) {
+            this.Name = dbName;
+        }
+    }
+}
---
+++ Packages/com.tivadar.best.http/Runtime/Shared/Database/MetadataIndexFinders/IEmptyMetadataIndexFinder.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+
+namespace Best.HTTP.Shared.Databases.MetadataIndexFinders {
+    public interface IEmptyMetadataIndexFinder<MetadataType> where MetadataType : Metadata {
+        int FindFreeIndex(List<MetadataType> metadatas);
+    }
+}
