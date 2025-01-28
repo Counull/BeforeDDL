48e21f914ca144384361e4c0f2523cd8420565cb
liuh <counull@163.com>
2025-01-28 23:27:53 +0800

    feat(Launcher): add LoadSceneProcess class with Start method
    - Introduced LoadSceneProcess class in the Launcher namespace.
    - Implemented the Start method as an override from AsyncProcess.

--- Assets/Scripts/C-Sharp/BaseAbstract/Process.cs
+++ Assets/Scripts/C-Sharp/BaseAbstract/Process.cs
@@ -1,3 +1,31 @@
-﻿namespace C_Sharp.BaseAbstract {
-    public class Process { }
+﻿using System.Collections;
+using UnityEngine;
+
+namespace C_Sharp.BaseAbstract {
+    public abstract class Process {
+        public abstract void Start();
+
+        protected virtual void Complete() {
+            _isComplete = true;
+        }
+
+        private bool _isComplete;
+
+        public bool IsComplete => _isComplete;
+    }
+
+    public abstract class AsyncProcess : Process {
+        public delegate void OnProcessComplete();
+
+        public virtual event OnProcessComplete OnComplete;
+
+        protected override void Complete() {
+            base.Complete();
+            OnComplete?.Invoke();
+        }
+
+        IEnumerator WaitingForProcess() {
+            yield return new WaitUntil(() => IsComplete);
+        }
+    }
 }
---
+++ Assets/Scripts/C-Sharp/Launcher/LoadSceneProcess.cs
@@ -0,0 +1,7 @@
+﻿using C_Sharp.BaseAbstract;
+
+namespace C_Sharp.Launcher {
+    public class LoadSceneProcess : AsyncProcess {
+        public override void Start() { }
+    }
+}
